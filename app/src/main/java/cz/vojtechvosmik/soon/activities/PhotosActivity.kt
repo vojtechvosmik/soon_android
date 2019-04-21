e3850ad14ce64a0c24a87988377fb044bb4b76d1
Vojtěch Vošmík <vosmik.vojtech@gmail.com>
2019-04-21 08:33:21 +0200

    photo from gallery

--- app/src/main/java/cz/vojtechvosmik/soon/activities/PhotosActivity.kt
+++ app/src/main/java/cz/vojtechvosmik/soon/activities/PhotosActivity.kt
@@ -11,12 +11,18 @@ import cz.vojtechvosmik.soon.utils.StorageUtils
 import kotlinx.android.synthetic.main.activity_photos.*
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.view.View
+import android.widget.Toast
 import cz.vojtechvosmik.soon.singletons.PhotoSingleton
 import kotlinx.android.synthetic.main.view_toolbar.view.*
+import java.lang.Exception
 
 class PhotosActivity : AppCompatActivity() {
+
+    private val GALLERY_PHOTOS_REQUEST_CODE = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +34,9 @@ class PhotosActivity : AppCompatActivity() {
 
     private fun setupViews() {
         recycler_photos.layoutManager = GridLayoutManager(this, 3)
+        fab_photo_library.setOnClickListener {
+            selectPhotoFromGallery()
+        }
     }
 
     private fun setupToolbar() {
@@ -45,10 +54,35 @@ class PhotosActivity : AppCompatActivity() {
 
             override fun onPhotoSelected(photo: Drawable) {
                 val photoBitmap = (photo as BitmapDrawable).bitmap
-                PhotoSingleton.selectedPhoto = photoBitmap
-                setResult(Activity.RESULT_OK, Intent())
-                finish()
+                returnPhoto(photoBitmap)
             }
         })
+    }
+
+    private fun selectPhotoFromGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.choose_photo)), GALLERY_PHOTOS_REQUEST_CODE)
+    }
+
+    private fun returnPhoto(photoBitmap: Bitmap) {
+        PhotoSingleton.selectedPhoto = photoBitmap
+        setResult(Activity.RESULT_OK, Intent())
+        finish()
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == GALLERY_PHOTOS_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            try {
+                val inputStream = contentResolver.openInputStream(data?.data!!)
+                val options = BitmapFactory.Options()
+                options.inSampleSize = 5
+                val photoBitmap = BitmapFactory.decodeStream(inputStream, null, options)
+                returnPhoto(photoBitmap!!)
+            }catch (e: Exception) {
+                Toast.makeText(this, getString(R.string.error_photo_from_gallery), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
